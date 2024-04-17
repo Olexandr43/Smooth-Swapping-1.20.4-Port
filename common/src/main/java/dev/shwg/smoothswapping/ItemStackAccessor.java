5155135fc8ab0b004b651346b412ec340ee327a0
Olexandr43 <161527365+Olexandr43@users.noreply.github.com>
2024-04-17 15:06:43 +0300

    Delete common directory

--- common/src/main/java/dev/shwg/smoothswapping/ItemStackAccessor.java
+++
@@ -1,8 +0,0 @@
-package dev.shwg.smoothswapping;
-
-public interface ItemStackAccessor {
-
-    void smooth_Swapping$setIsSwapStack(boolean isSwapStack);
-
-    boolean smooth_Swapping$isSwapStack();
-}
